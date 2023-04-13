b74d267d53044c47891936797a1738a98705dc11
Mike K <42897361+Medard22@users.noreply.github.com>
2023-04-13 17:05:31 +0200

    Update DirectoryInitialization.java

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/DirectoryInitialization.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/DirectoryInitialization.java
@@ -54,7 +54,7 @@ public final class DirectoryInitialization
     directoryState.setValue(DirectoryInitializationState.INITIALIZING);
 
     // Can take a few seconds to run, so don't block UI thread.
-    new Thread(() -> init(context)).run();
+    ((Runnable) () -> init(context)).run();
   }
 
   private static void init(Context context)
