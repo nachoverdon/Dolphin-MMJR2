dolphin-emu/dolphin

DolphinQt: Set window decorations for all top-level QWidgets.

Source/Core/DolphinQt/TAS/WiiTASInputWindow.cpp
@@ -28,6 +28,7 @@
 
 #include "DolphinQt/QtUtils/AspectRatioWidget.h"
 #include "DolphinQt/QtUtils/QueueOnObject.h"
+#include "DolphinQt/QtUtils/SetWindowDecorations.h"
 #include "DolphinQt/TAS/IRWidget.h"
 #include "DolphinQt/TAS/TASCheckBox.h"
 #include "DolphinQt/TAS/TASSpinBox.h"
@@ -395,15 +396,21 @@ void WiiTASInputWindow::UpdateExt()
   if (m_active_extension == WiimoteEmu::ExtensionNumber::NUNCHUK)
   {
     setWindowTitle(tr("Wii TAS Input %1 - Wii Remote + Nunchuk").arg(m_num + 1));
+    SetQWidgetWindowDecorations(m_ir_box);
     m_ir_box->show();
+    SetQWidgetWindowDecorations(m_nunchuk_stick_box);
     m_nunchuk_stick_box->show();
     m_classic_right_stick_box->hide();
     m_classic_left_stick_box->hide();
+    SetQWidgetWindowDecorations(m_remote_accelerometer_box);
     m_remote_accelerometer_box->show();
     m_remote_gyroscope_box->setVisible(m_is_motion_plus_attached);
+    SetQWidgetWindowDecorations(m_nunchuk_accelerometer_box);
     m_nunchuk_accelerometer_box->show();
     m_triggers_box->hide();
+    SetQWidgetWindowDecorations(m_nunchuk_buttons_box);
     m_nunchuk_buttons_box->show();
+    SetQWidgetWindowDecorations(m_remote_buttons_box);
     m_remote_buttons_box->show();
     m_classic_buttons_box->hide();
   }
@@ -412,14 +419,18 @@ void WiiTASInputWindow::UpdateExt()
     setWindowTitle(tr("Wii TAS Input %1 - Classic Controller").arg(m_num + 1));
     m_ir_box->hide();
     m_nunchuk_stick_box->hide();
+    SetQWidgetWindowDecorations(m_classic_right_stick_box);
     m_classic_right_stick_box->show();
+    SetQWidgetWindowDecorations(m_classic_left_stick_box);
     m_classic_left_stick_box->show();
     m_remote_accelerometer_box->hide();
     m_remote_gyroscope_box->hide();
     m_nunchuk_accelerometer_box->hide();
+    SetQWidgetWindowDecorations(m_triggers_box);
     m_triggers_box->show();
     m_remote_buttons_box->hide();
     m_nunchuk_buttons_box->hide();
+    SetQWidgetWindowDecorations(m_classic_buttons_box);
     m_classic_buttons_box->show();
   }
   else
@@ -429,10 +440,12 @@ void WiiTASInputWindow::UpdateExt()
     m_nunchuk_stick_box->hide();
     m_classic_right_stick_box->hide();
     m_classic_left_stick_box->hide();
+    SetQWidgetWindowDecorations(m_remote_accelerometer_box);
     m_remote_accelerometer_box->show();
     m_remote_gyroscope_box->setVisible(m_is_motion_plus_attached);
     m_nunchuk_accelerometer_box->hide();
     m_triggers_box->hide();
+    SetQWidgetWindowDecorations(m_remote_buttons_box);
     m_remote_buttons_box->show();
     m_nunchuk_buttons_box->hide();
     m_classic_buttons_box->hide();

nachoverdon/Dolphin-MMJR2

Source/Core/DolphinQt/TAS/WiiTASInputWindow.cpp
@@ -31,6 +31,7 @@
 
 #include "DolphinQt/QtUtils/AspectRatioWidget.h"
 #include "DolphinQt/QtUtils/QueueOnObject.h"
+#include "DolphinQt/QtUtils/SetWindowDecorations.h"
 #include "DolphinQt/TAS/IRWidget.h"
 #include "DolphinQt/TAS/TASCheckBox.h"
 
@@ -273,14 +274,20 @@ void WiiTASInputWindow::UpdateExt(u8 ext)
   if (ext == 1)
   {
     setWindowTitle(tr("Wii TAS Input %1 - Wii Remote + Nunchuk").arg(m_num + 1));
+    SetQWidgetWindowDecorations(m_ir_box);
     m_ir_box->show();
+    SetQWidgetWindowDecorations(m_nunchuk_stick_box);
     m_nunchuk_stick_box->show();
     m_classic_right_stick_box->hide();
     m_classic_left_stick_box->hide();
+    SetQWidgetWindowDecorations(m_remote_orientation_box);
     m_remote_orientation_box->show();
+    SetQWidgetWindowDecorations(m_nunchuk_orientation_box);
     m_nunchuk_orientation_box->show();
     m_triggers_box->hide();
+    SetQWidgetWindowDecorations(m_nunchuk_buttons_box);
     m_nunchuk_buttons_box->show();
+    SetQWidgetWindowDecorations(m_remote_buttons_box);
     m_remote_buttons_box->show();
     m_classic_buttons_box->hide();
   }
@@ -289,13 +296,17 @@ void WiiTASInputWindow::UpdateExt(u8 ext)
     setWindowTitle(tr("Wii TAS Input %1 - Classic Controller").arg(m_num + 1));
     m_ir_box->hide();
     m_nunchuk_stick_box->hide();
+    SetQWidgetWindowDecorations(m_classic_right_stick_box);
     m_classic_right_stick_box->show();
+    SetQWidgetWindowDecorations(m_classic_left_stick_box);
     m_classic_left_stick_box->show();
     m_remote_orientation_box->hide();
     m_nunchuk_orientation_box->hide();
+    SetQWidgetWindowDecorations(m_triggers_box);
     m_triggers_box->show();
     m_remote_buttons_box->hide();
     m_nunchuk_buttons_box->hide();
+    SetQWidgetWindowDecorations(m_classic_buttons_box);
     m_classic_buttons_box->show();
   }
   else
@@ -305,9 +316,11 @@ void WiiTASInputWindow::UpdateExt(u8 ext)
     m_nunchuk_stick_box->hide();
     m_classic_right_stick_box->hide();
     m_classic_left_stick_box->hide();
+    SetQWidgetWindowDecorations(m_remote_orientation_box);
     m_remote_orientation_box->show();
     m_nunchuk_orientation_box->hide();
     m_triggers_box->hide();
+    SetQWidgetWindowDecorations(m_remote_buttons_box);
     m_remote_buttons_box->show();
     m_nunchuk_buttons_box->hide();
     m_classic_buttons_box->hide();